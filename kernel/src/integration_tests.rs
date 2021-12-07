58f44eb87b51db9ac76f1233f9b27151389b234b
Erika Hunhoff <hunhoffe@vmware.com>
2021-12-07 13:00:21 -0800

    Fixed controller test name to fit new integration test model, also fixed formatting

--- kernel/src/integration_tests.rs
+++ kernel/src/integration_tests.rs
@@ -710,12 +710,13 @@ fn vmxnet_smoltcp() {
 /// Test TCP RPC-based controller
 #[cfg(all(feature = "integration-test", target_arch = "x86_64"))]
 fn controller() {
-    use rpc::tcp_server::TCPServer;
-    use rpc::rpc_api::RPCServerAPI;
     use rpc::cluster_api::ClusterControllerAPI;
     use rpc::rpc::RPCType;
-    use crate::arch::network::init_network;
+    use rpc::rpc_api::RPCServerAPI;
+    use rpc::tcp_server::TCPServer;
+
     use crate::arch::exokernel::*;
+    use crate::arch::network::init_network;
 
     const PORT: u16 = 6970;
 
@@ -724,17 +725,28 @@ fn controller() {
 
     // TODO: register handlers
     let (server, _) = server
-        .register(FileIO::Close as RPCType, &CLOSE_HANDLER).unwrap()
-        .register(FileIO::Delete as RPCType, &DELETE_HANDLER).unwrap()
-        .register(FileIO::GetInfo as RPCType, &GETINFO_HANDLER).unwrap()
-        .register(FileIO::MkDir as RPCType, &MKDIR_HANDLER).unwrap()
-        .register(FileIO::Open as RPCType, &OPEN_HANDLER).unwrap()
-        .register(FileIO::FileRename as RPCType, &RENAME_HANDLER).unwrap()
-        .register(FileIO::Write as RPCType, &WRITE_HANDLER).unwrap()
-        .register(FileIO::WriteAt as RPCType, &WRITE_HANDLER).unwrap()
-        .register(FileIO::Read as RPCType, &READ_HANDLER).unwrap()
-        .register(FileIO::ReadAt as RPCType, &READ_HANDLER).unwrap()
-        .add_client(&CLIENT_REGISTRAR).unwrap();
+        .register(FileIO::Close as RPCType, &CLOSE_HANDLER)
+        .unwrap()
+        .register(FileIO::Delete as RPCType, &DELETE_HANDLER)
+        .unwrap()
+        .register(FileIO::GetInfo as RPCType, &GETINFO_HANDLER)
+        .unwrap()
+        .register(FileIO::MkDir as RPCType, &MKDIR_HANDLER)
+        .unwrap()
+        .register(FileIO::Open as RPCType, &OPEN_HANDLER)
+        .unwrap()
+        .register(FileIO::FileRename as RPCType, &RENAME_HANDLER)
+        .unwrap()
+        .register(FileIO::Write as RPCType, &WRITE_HANDLER)
+        .unwrap()
+        .register(FileIO::WriteAt as RPCType, &WRITE_HANDLER)
+        .unwrap()
+        .register(FileIO::Read as RPCType, &READ_HANDLER)
+        .unwrap()
+        .register(FileIO::ReadAt as RPCType, &READ_HANDLER)
+        .unwrap()
+        .add_client(&CLIENT_REGISTRAR)
+        .unwrap();
     server.run_server().unwrap();
 
     shutdown(ExitReason::Ok);
